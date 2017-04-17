38bb9d40c50f0b237788353478107d3bdc9bae72
A A <abcdefghijklm@outlook.jp>
2017-04-17 21:14:29 +0900

    initial commit

---
+++ src/main/java/jp/ac/hiroshima_u/fu_midori/SSH2017/test2/App.java
@@ -0,0 +1,13 @@
+package jp.ac.hiroshima_u.fu_midori.SSH2017.test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
