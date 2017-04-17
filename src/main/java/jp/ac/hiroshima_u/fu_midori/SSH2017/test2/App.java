61b24583f68c448af6eb4a340d334466cd52d9b3
A A <abcdefghijklm@outlook.jp>
2017-04-17 21:35:06 +0900

    prtest

--- src/main/java/jp/ac/hiroshima_u/fu_midori/SSH2017/test2/App.java
+++ src/main/java/jp/ac/hiroshima_u/fu_midori/SSH2017/test2/App.java
@@ -1,13 +1,8 @@
 package jp.ac.hiroshima_u.fu_midori.SSH2017.test2;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Other Message!");
+	}
 }
